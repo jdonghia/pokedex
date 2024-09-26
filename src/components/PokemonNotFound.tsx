de4cee95022d347cb037b36f248e1a45a0c128c7
Joao Donghia <78510025+jdonghia@users.noreply.github.com>
2024-09-26 18:04:52 -0300

    style(PokemonNotFound): adjust style

--- src/components/PokemonNotFound.tsx
+++ src/components/PokemonNotFound.tsx
@@ -1,7 +1,7 @@
 export function PokemonNotFound() {
   return (
-    <div className="col-[span_36_/_span_36] row-[span_32_/_span_32] grid place-content-center">
-      <div className="flex flex-col items-center">
+    <div className="col-[span_36_/_span_36] row-[span_32_/_span_32] grid place-content-center bg-[#e8e8e8]">
+      <div className="flex flex-col items-center text-[#1a1a1a]">
         <span className="text-9xl font-bold">404</span>
         <p>Sorry, we couldnt find any pokemon match</p>
         <p>Click on reload to reset filters</p>
